2c4463e3fe4936e11eb88e9a8b31be132f15caa4
Shrey-1123 <you@example.com>
2021-06-09 01:38:41 +0530

    home content edited

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public replay: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  animating() { console.log('Animating...'); }
+  animated() { console.log('Animated'); }
+
+ 
+
+  readonly content = {
+
+    top: { 
+      title: "Get your single page application done right",
+      description: "Wizdm provides all the key features of a modern single page application ready to use, so, for you to focus on launching your idea the quickest!", 
+      image: "assets/img/blue-buddha.svg"
+    }
+
+   
+  };
 
 }
